f9147b4bde757ef04e23f4d2a46d259a0a5334c5
akash <a8892242921@gmail.com>
2026-03-25 16:56:00 +0530

    changed bcrypt to bcryptjs

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // app.js
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const { Pool } = require('pg');
